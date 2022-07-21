de952c3c45f1f10a6eabeaac6ae4b87b7b51fb52
yunjin544 <yunjin544@gmail.com>
2022-07-21 11:11:50 +0900

    test

--- Hello_World/main.py
+++ Hello_World/main.py
@@ -1,21 +1,16 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-
-
-class MyApp(QWidget):
-
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+ 
+form_class = uic.loadUiType("Hello_world.ui")[0]
+ 
+class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.setWindowTitle('My First Application')
-        self.move(300, 300)
-        self.resize(400, 200)
-        self.show()
-
-
-if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+        self.setupUi(self)
+ 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
